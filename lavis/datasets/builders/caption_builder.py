ff560eedf58b9c5c7e292c8fbd488f6d08af1630
SunzeYi <983499284@qq.com>
2023-08-13 18:03:00 +0800

    add vqa demo

--- lavis/datasets/builders/caption_builder.py
+++ lavis/datasets/builders/caption_builder.py
@@ -6,86 +6,86 @@
 """
 
 from lavis.datasets.builders.base_dataset_builder import BaseDatasetBuilder
-from lavis.datasets.datasets.coco_caption_datasets import (
-    COCOCapDataset,
-    COCOCapEvalDataset,
-    NoCapsEvalDataset,
-)
+# from lavis.datasets.datasets.coco_caption_datasets import (
+#     COCOCapDataset,
+#     COCOCapEvalDataset,
+#     NoCapsEvalDataset,
+# )
 
-from lavis.common.registry import registry
-from lavis.datasets.datasets.video_caption_datasets import (
-    VideoCaptionDataset,
-    VideoCaptionEvalDataset,
-)
+# from lavis.common.registry import registry
+# from lavis.datasets.datasets.video_caption_datasets import (
+#     VideoCaptionDataset,
+#     VideoCaptionEvalDataset,
+# )
 
-from lavis.datasets.datasets.shapenet_caption_datasets import (
-    ShapeNetCapDataset,
-    ShapeNetCapEvalDataset,
-    NoCapsEvalDataset,
-)
+# from lavis.datasets.datasets.shapenet_caption_datasets import (
+#     ShapeNetCapDataset,
+#     ShapeNetCapEvalDataset,
+#     NoCapsEvalDataset,
+# )
 
 from lavis.datasets.datasets.objaverse_caption_datasets import (
     ObjaverseCapDataset,
     ObjaverseCapDataset_tune,
     ObjaverseCapEvalDataset,
 )
+from lavis.common.registry import registry
+# @registry.register_builder("coco_caption")
+# class COCOCapBuilder(BaseDatasetBuilder):
+#     train_dataset_cls = COCOCapDataset
+#     eval_dataset_cls = COCOCapEvalDataset
 
-@registry.register_builder("coco_caption")
-class COCOCapBuilder(BaseDatasetBuilder):
-    train_dataset_cls = COCOCapDataset
-    eval_dataset_cls = COCOCapEvalDataset
-
-    DATASET_CONFIG_DICT = {
-        "default": "configs/datasets/coco/defaults_cap.yaml",
-    }
-
-
-@registry.register_builder("nocaps")
-class COCOCapBuilder(BaseDatasetBuilder):
-    eval_dataset_cls = NoCapsEvalDataset
-
-    DATASET_CONFIG_DICT = {
-        "default": "configs/datasets/nocaps/defaults.yaml",
-    }
+#     DATASET_CONFIG_DICT = {
+#         "default": "configs/datasets/coco/defaults_cap.yaml",
+#     }
 
 
-@registry.register_builder("msrvtt_caption")
-class MSRVTTCapBuilder(BaseDatasetBuilder):
-    train_dataset_cls = VideoCaptionDataset
-    eval_dataset_cls = VideoCaptionEvalDataset
+# @registry.register_builder("nocaps")
+# class COCOCapBuilder(BaseDatasetBuilder):
+#     eval_dataset_cls = NoCapsEvalDataset
 
-    DATASET_CONFIG_DICT = {
-        "default": "configs/datasets/msrvtt/defaults_cap.yaml",
-    }
-
-
-@registry.register_builder("msvd_caption")
-class MSVDCapBuilder(BaseDatasetBuilder):
-    train_dataset_cls = VideoCaptionDataset
-    eval_dataset_cls = VideoCaptionEvalDataset
-
-    DATASET_CONFIG_DICT = {
-        "default": "configs/datasets/msvd/defaults_cap.yaml",
-    }
+#     DATASET_CONFIG_DICT = {
+#         "default": "configs/datasets/nocaps/defaults.yaml",
+#     }
 
 
-@registry.register_builder("vatex_caption")
-class VATEXCapBuilder(BaseDatasetBuilder):
-    train_dataset_cls = VideoCaptionDataset
-    eval_dataset_cls = VideoCaptionEvalDataset
+# @registry.register_builder("msrvtt_caption")
+# class MSRVTTCapBuilder(BaseDatasetBuilder):
+#     train_dataset_cls = VideoCaptionDataset
+#     eval_dataset_cls = VideoCaptionEvalDataset
 
-    DATASET_CONFIG_DICT = {
-        "default": "configs/datasets/vatex/defaults_cap.yaml",
-    }
+#     DATASET_CONFIG_DICT = {
+#         "default": "configs/datasets/msrvtt/defaults_cap.yaml",
+#     }
 
-@registry.register_builder("shapenet_caption")
-class ShapeNetCapBuilder(BaseDatasetBuilder):
-    train_dataset_cls = ShapeNetCapDataset
-    eval_dataset_cls = ShapeNetCapEvalDataset
 
-    DATASET_CONFIG_DICT = {
-        "default": "configs/datasets/shapenet/defaults_cap.yaml",
-    }
+# @registry.register_builder("msvd_caption")
+# class MSVDCapBuilder(BaseDatasetBuilder):
+#     train_dataset_cls = VideoCaptionDataset
+#     eval_dataset_cls = VideoCaptionEvalDataset
+
+#     DATASET_CONFIG_DICT = {
+#         "default": "configs/datasets/msvd/defaults_cap.yaml",
+#     }
+
+
+# @registry.register_builder("vatex_caption")
+# class VATEXCapBuilder(BaseDatasetBuilder):
+#     train_dataset_cls = VideoCaptionDataset
+#     eval_dataset_cls = VideoCaptionEvalDataset
+
+#     DATASET_CONFIG_DICT = {
+#         "default": "configs/datasets/vatex/defaults_cap.yaml",
+#     }
+
+# @registry.register_builder("shapenet_caption")
+# class ShapeNetCapBuilder(BaseDatasetBuilder):
+#     train_dataset_cls = ShapeNetCapDataset
+#     eval_dataset_cls = ShapeNetCapEvalDataset
+
+#     DATASET_CONFIG_DICT = {
+#         "default": "configs/datasets/shapenet/defaults_cap.yaml",
+#     }
 
 @registry.register_builder("objaverse_caption")
 class ObjaverseCapBuilder(BaseDatasetBuilder):
@@ -97,7 +97,7 @@ class ObjaverseCapBuilder(BaseDatasetBuilder):
     }
 
 @registry.register_builder("objaverse_caption_tune")
-class ObjaverseCapBuilder(BaseDatasetBuilder):
+class ObjaverseCap_tune_Builder(BaseDatasetBuilder):
     train_dataset_cls = ObjaverseCapDataset_tune
     eval_dataset_cls = ObjaverseCapEvalDataset
 
