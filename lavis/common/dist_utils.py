5ca20947001bdf657f82c52e8237f430447fd2f8
VeryGreatGuy <60505198+zhaozh10@users.noreply.github.com>
2023-08-17 00:10:35 +0800

    enable slurm implementation

--- lavis/common/dist_utils.py
+++ lavis/common/dist_utils.py
@@ -8,7 +8,7 @@
 import datetime
 import functools
 import os
-
+import subprocess
 import torch
 import torch.distributed as dist
 import timm.models.hub as timm_hub
@@ -53,39 +53,105 @@ def get_rank():
 def is_main_process():
     return get_rank() == 0
 
+def infer_launcher():
+    if 'WORLD_SIZE' in os.environ:
+        return 'pytorch'
+    elif 'SLURM_NTASKS' in os.environ:
+        return 'slurm'
+    elif 'OMPI_COMM_WORLD_LOCAL_RANK' in os.environ:
+        return 'mpi'
+    else:
+        return 'none'
+
+def _init_dist_slurm(args, backend,
+                     port=None,
+                     init_backend='torch',
+                     **kwargs) -> None:
+    """Initialize slurm distributed training environment.
+
+    If argument ``port`` is not specified, then the master port will be system
+    environment variable ``MASTER_PORT``. If ``MASTER_PORT`` is not in system
+    environment variable, then a default port ``29500`` will be used.
+
+    Args:
+        backend (str): Backend of torch.distributed.
+        port (int, optional): Master port. Defaults to None.
+    """
+    proc_id = int(os.environ['SLURM_PROCID'])
+    ntasks = int(os.environ['SLURM_NTASKS'])
+    node_list = os.environ['SLURM_NODELIST']
+
+    args.rank = proc_id
+    args.gpu = args.rank % torch.cuda.device_count()
+    # Not sure when this environment variable could be None, so use a fallback
+    local_rank_env = os.environ.get('SLURM_LOCALID', None)
+    if local_rank_env is not None:
+        local_rank = int(local_rank_env)
+    else:
+        num_gpus = torch.cuda.device_count()
+        local_rank = proc_id % num_gpus
+    torch.cuda.set_device(local_rank)
+    addr = subprocess.getoutput(
+        f'scontrol show hostname {node_list} | head -n1')
+    # specify master port
+    if port is not None:
+        os.environ['MASTER_PORT'] = str(port)
+    elif 'MASTER_PORT' in os.environ:
+        pass  # use MASTER_PORT in the environment variable
+    else:
+        # 29500 is torch.distributed default port
+        os.environ['MASTER_PORT'] = '29500'
+    # use MASTER_ADDR in the environment variable if it already exists
+    if 'MASTER_ADDR' not in os.environ:
+        os.environ['MASTER_ADDR'] = addr
+    os.environ['WORLD_SIZE'] = str(ntasks)
+    os.environ['LOCAL_RANK'] = str(local_rank)
+    os.environ['RANK'] = str(proc_id)
+
+    if init_backend == 'torch':
+        dist.init_process_group(backend=backend, **kwargs)
+    else:
+        raise ValueError('supported "init_backend" is "torch" or "deepspeed", '
+                         f'but got {init_backend}')
+
 
 def init_distributed_mode(args):
-    if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
-        args.rank = int(os.environ["RANK"])
-        args.world_size = int(os.environ["WORLD_SIZE"])
-        args.gpu = int(os.environ["LOCAL_RANK"])
-    elif "SLURM_PROCID" in os.environ:
-        args.rank = int(os.environ["SLURM_PROCID"])
-        args.gpu = args.rank % torch.cuda.device_count()
-    else:
-        print("Not using distributed mode")
-        args.distributed = False
-        return
+    # if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
+    #     args.rank = int(os.environ["RANK"])
+    #     args.world_size = int(os.environ["WORLD_SIZE"])
+    #     args.gpu = int(os.environ["LOCAL_RANK"])
+    # elif "SLURM_PROCID" in os.environ:
+    #     print("slurm!!!")
+    #     args.rank = int(os.environ["SLURM_PROCID"])
+    #     args.gpu = args.rank % torch.cuda.device_count()
+    # else:
+    #     print("Not using distributed mode")
+    #     args.distributed = False
+    #     return
+
+    launcher=infer_launcher()
 
     args.distributed = True
-
-    torch.cuda.set_device(args.gpu)
     args.dist_backend = "nccl"
-    print(
-        "| distributed init (rank {}, world {}): {}".format(
-            args.rank, args.world_size, args.dist_url
-        ),
-        flush=True,
-    )
-    torch.distributed.init_process_group(
-        backend=args.dist_backend,
-        init_method=args.dist_url,
-        world_size=args.world_size,
-        rank=args.rank,
-        timeout=datetime.timedelta(
-            days=365
-        ),  # allow auto-downloading and de-compressing
-    )
+    if launcher == 'slurm':
+        _init_dist_slurm(args=args,backend=args.dist_backend, init_backend='torch')
+    else:
+        print(
+            "| distributed init (rank {}, world {}): {}".format(
+                args.rank, args.world_size, args.dist_url
+            ),
+            flush=True,
+        )
+        torch.cuda.set_device(args.gpu)
+        torch.distributed.init_process_group(
+            backend=args.dist_backend,
+            init_method=args.dist_url,
+            world_size=args.world_size,
+            rank=args.rank,
+            timeout=datetime.timedelta(
+                days=365
+            ),  # allow auto-downloading and de-compressing
+        )
     torch.distributed.barrier()
     setup_for_distributed(args.rank == 0)
 
